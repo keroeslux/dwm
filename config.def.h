9dcb3a611a1ec8b7ba8cf6cacbc02a901a0f6f4e
keroeslux <keroeslux@keroeslux-artix>
2022-06-27 21:53:23 +0000

    Added new keybind

--- config.def.h
+++ config.def.h
@@ -74,7 +74,7 @@ static Key keys[] = {
     { MODKEY,                       XK_u,      spawn,          SHCMD("firefox") },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_h,      spawn,          SHCMD("flameshot gui") },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
