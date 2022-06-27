df06d64820bdae9b32bc4204500fcee4d1e3687f
keroeslux <keroeslux@keroeslux-artix>
2022-06-27 20:39:46 +0000

    Added another patch

--- config.def.h
+++ config.def.h
@@ -8,6 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 15;       /* vertical padding of bar */
 static const int sidepad            = 15;       /* horizontal padding of bar */
+static const int user_bh            = 2;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "monospace:size=15" };
 static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#222222";
